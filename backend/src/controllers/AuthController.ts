8072b9f96292e5a3f632258185e010da6696f12c
Hugo Silva <hugo.f.silva15@aluno.senai.br>
2026-07-24 15:37:26 -0300

    Ajuste nos middlewares e adição do jwt

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,10 @@
 import type { Request, Response } from "express"
 import prisma from "../lib/prisma.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config.js'
+
+const SECRET_JWT = process.env.SECRET_JWT || ""
 
 export const Login = async (req:Request,res:Response) =>{
     const { email, password } = req.body
@@ -12,7 +16,11 @@ export const Login = async (req:Request,res:Response) =>{
         if(!user) return res.status(400).json({mensagem:"Email ou senha incorretos!"})
         const passwordHashed = await bcrypt.compare(String(password),user.senhaHash)
         if(!passwordHashed) return res.status(400).json({mensagem:"Email ou senha incorretos!"})
-        res.status(200).json({mensagem:"Login efetuado com sucesso!",token:""})
+        const userId = user.id
+        const userRole = user.cargo
+
+        const token = jwt.sign({userId,userRole},SECRET_JWT,{expiresIn:'1h'})
+        res.status(200).json({mensagem:"Login efetuado com sucesso!",token:token})
     } catch (error) {
         console.log(error)
         res.status(500).json({mensagem:"Erro ao fazer login!"})
--- backend/src/middlewares/middleware.ts
+++ backend/src/middlewares/middleware.ts
@@ -1,25 +1,35 @@
 import type { Request, Response, NextFunction } from "express"
+import jwt from 'jsonwebtoken'
+import 'dotenv/config.js'
 
 interface RequestExtends extends Request{
     user?:any
 }
 
+const SECRET_JWT = process.env.SECRET_JWT || ""
+
+
 export const login = async (req:RequestExtends,res:Response,next:NextFunction) =>{
     const authHeader = req.headers.authorization
 
-    if(!authHeader) return res.status(400).json({mensagem:"Precisa estar logado para fazer está ação!"})
+    if(!authHeader) return res.status(401).json({mensagem:"Token não fornecido!"})
 
 
-    const [ token,bearer ] = authHeader.split(' ')
+    const [ token,schema ] = authHeader.split(' ')
 
-    if(!token || !bearer) return res.status(400).json({mensagem:"Token mal formatado!"})
-
-    req.user = token
-    return next
+    if(!token || schema !== "Bearer") return res.status(401).json({mensagem:"Token mal formatado!"})
+    
+    try {
+        const decode = jwt.verify(token,SECRET_JWT)
+        req.user = decode
+        return next()
+    } catch (error) {
+        res.status(401).json({mensagem:"Token invalido ou expirado!"})
+    }
 }
 
 export const verifyInstructor = async (req:RequestExtends,res:Response,next:NextFunction) =>{
     if(req.user.role !== "Instrutor") return res.status(400).json({mensagem:"Apenas intrutor pode fazer essa ação!"})
 
-    return next
+    return next()
 }
